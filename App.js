4f71892034f752a0677184bfc08ee7b2a6ac2237
Rakib Raza <rakibraza159@gmail.com>
2021-06-05 15:03:15 +0600

    navbar complete

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from 'expo-constants'
+import NavBar from "./components/NavBar";
 export default function App() {
   console.log('rakib raza')
 
   return (
     <View style={styles.container}>
-
+      <NavBar />
     </View>
   );
 }
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, StyleSheet } from 'react-native'
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons'
 const NavBar = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <FontAwesome5 name="fire" size={27} color="#6644F7" />
+      <FontAwesome name="comments" size={27} color="#000" />
+      <FontAwesome name="user" size={27} color="#000" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 15,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.12,
+    shadowRadius: 5.46,
+    elevation: 9,
+  }
+})
+
+
+
+
 
 export default NavBar
