06c2a74fe9699da3190ce40ee0362caa431f4705
Rakib Raza <rakibraza159@gmail.com>
2021-06-05 15:33:15 +0600

    fetch friends

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from 'expo-constants'
 import NavBar from "./components/NavBar";
+import axios from 'axios'
+
 export default function App() {
-  console.log('rakib raza')
+  const [friends, setFriends] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  async function fetchFriends() {
+    try {
+      const { data } = await axios.get('https://randomuser.me/api/?results=40')
+      setFriends(data.results)
+      console.log(data.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchFriends()
+  }, [])
 
   return (
     <View style={styles.container}>
       <NavBar />
+      <View style={styles.swipes}>
+
+      </View>
     </View>
   );
 }
@@ -17,5 +36,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight
+  },
+  swipes: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
 });
